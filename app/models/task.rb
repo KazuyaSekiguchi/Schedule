87b62dd037c2cfead77cf698cc5a2e26c3891db8
KazuyaSekiguchi <kzy.sekiguchi0908@gmail.com>
2023-05-04 14:18:34 +0000

    rails課題質問

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,14 +1,29 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 20 }
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates :start_date, presence: true, unless: -> { start_date.blank? }
+  validates :end_date, presence: true, unless: -> { end_date.blank? }
+  
+  
   
   validate :start_end_check
   def start_end_check
-    errors.add(:end_date, "の日付を正しく記入してください") unless
-    self.start_date < self.end_date
+    if start_date.present?
+    errors.add(:end_date, "の日付を正しく記入してください") 
+    start_date > end_date
+    end
+  end
+  
+  validate :startdate
+  def startdate
+    errors.add(:start_date, "の日付が空欄です") if 
+    start_date == nil
+  end
+  
+  validate :enddate
+  def enddate
+    errors.add(:end_date, "の日付が空欄です") if 
+    end_date == nil
   end
   
   validates :memo, length: { maximum: 500 }
-  
 end
